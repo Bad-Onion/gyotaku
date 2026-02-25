9e95149cbc320132f61ffef9bb26f61155775f11
Neronna <sofiagabirausch@gmail.com>
2026-02-25 18:19:18 -0300

    Dados do catálogo placeholder prontos, sistema básico de dar nome pros peixes

--- gyotaku/scenes/minigames/catalogo_manager.gd
+++ gyotaku/scenes/minigames/catalogo_manager.gd
@@ -2,9 +2,14 @@ extends Control
 
 @onready var peixe_catalogo: SubViewport = $PeixeCatalogo
 @onready var pintura: Sprite2D = $PeixeCatalogo/Peixe/Pintura
-@onready var peixe: TextureRect = $GridContainer/Peixe
+@onready var peixe: Sprite2D = $Peixe
+@onready var caixa_de_texto: TextEdit = $NomeDoPeixe
+const CAMINHO_SAVE = "user://nome_peixes.txt"
+var tipo : String
+var todos_os_nomes : Dictionary = {}
 
 func _ready() -> void:
+
 	Input.set_custom_mouse_cursor(null)
 
 	if Global.ultimo_peixe_carimbado != "":
@@ -12,9 +17,11 @@ func _ready() -> void:
 	else:
 		trocar_peixe_na_tela("gurukun")
 
-func trocar_peixe_na_tela(nome_do_peixe: String):
+func trocar_peixe_na_tela(novo_tipo: String):
+	tipo = novo_tipo
+	load_text(tipo)
 	# 1. Manda o SubViewport trocar as texturas internas dele
-	peixe_catalogo.atualizar_peixe(nome_do_peixe)
+	peixe_catalogo.atualizar_peixe(tipo)
 	
 	# 2. Pede pra atualizar a tela
 	adicionar_peixe_ao_catalogo()
@@ -24,3 +31,36 @@ func adicionar_peixe_ao_catalogo():
 	await get_tree().process_frame
 	# Pega o resultado mesclado e joga na cara do jogador
 	peixe.texture = peixe_catalogo.get_texture()
+
+
+func _on_nome_do_peixe_text_changed() -> void:
+	save_text(caixa_de_texto.text)
+
+func save_text(conteudo_digitado : String):
+	todos_os_nomes[tipo] = conteudo_digitado
+	var texto_json = JSON.stringify(todos_os_nomes)
+	
+	
+	var file = FileAccess.open(CAMINHO_SAVE, FileAccess.WRITE)
+	if file:
+		file.store_string(texto_json)
+		file.close()
+	else:
+		print("Erro em escrever no arquivo")
+
+func load_text(tipo_buscado : String):
+	if FileAccess.file_exists(CAMINHO_SAVE):
+		var file = FileAccess.open(CAMINHO_SAVE, FileAccess.READ)
+		if file:
+			var texto_do_arquivo = file.get_as_text()
+			file.close()
+			var json_convertido = JSON.parse_string(texto_do_arquivo)
+			if json_convertido != null and typeof(json_convertido) == TYPE_DICTIONARY:
+				todos_os_nomes = json_convertido
+			print("Texto carregado.")
+		else:
+			print("Erro para abrir o arquivo")
+	if todos_os_nomes.has(tipo_buscado):
+		caixa_de_texto.text = todos_os_nomes[tipo_buscado]
+	else:
+		caixa_de_texto.text = ""

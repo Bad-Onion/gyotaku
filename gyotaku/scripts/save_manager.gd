d120820d25f19f8a3906b4527cf3bca446d2167c
Neronna <sofiagabirausch@gmail.com>
2026-02-27 00:25:59 -0300

    Toques finais das HUDS e cheat de apertar enter por 10 segundos pra liberar todos os peixes

--- gyotaku/scripts/save_manager.gd
+++ gyotaku/scripts/save_manager.gd
@@ -1,9 +1,45 @@
 extends Node2D
 
+var tempo_segurando_enter : float = 0.0
+var cheat_ativado : bool = false
 const CAMINHO_SAVE = "user://save_do_jogo.json"
 
 func _ready() -> void:
 	inicializar_save_se_nao_existir()
+
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_ENTER) or Input.is_key_pressed(KEY_KP_ENTER):
+		if not cheat_ativado:
+			tempo_segurando_enter += delta
+			
+			if tempo_segurando_enter >= 10.0:
+				cheat_ativado = true
+				print("CHEAT ATIVADO: 10 segundos de Enter detectados!")
+				ativar_trapaca_pegar_todos()
+	else:
+		tempo_segurando_enter = 0.0
+
+func ativar_trapaca_pegar_todos() -> void:
+	var save_temporario : Dictionary = {}
+	
+	if FileAccess.file_exists(CAMINHO_SAVE):
+		var file = FileAccess.open(CAMINHO_SAVE, FileAccess.READ)
+		var json_convertido = JSON.parse_string(file.get_as_text())
+		file.close()
+		
+		if typeof(json_convertido) == TYPE_DICTIONARY:
+			save_temporario = json_convertido
+			
+			for chave_peixe in save_temporario.keys():
+				save_temporario[chave_peixe]["pego"] = true
+				
+			var texto_json = JSON.stringify(save_temporario, "\t")
+			var save_file = FileAccess.open(CAMINHO_SAVE, FileAccess.WRITE)
+			save_file.store_string(texto_json)
+			save_file.close()
+			
+			print("Todos os peixes liberados! Recarregando...")
+			get_tree().reload_current_scene()
 
 func inicializar_save_se_nao_existir() -> void:
 	if not FileAccess.file_exists(CAMINHO_SAVE):

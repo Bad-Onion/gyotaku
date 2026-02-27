c1ed2d16372a2005a7fad28de425ba080cc6f2d0
Neronna <sofiagabirausch@gmail.com>
2026-02-27 08:34:44 -0300

    Adicionado os sons de clique nos botões do menu

--- gyotaku/scenes/trans/transicao.gd
+++ gyotaku/scenes/trans/transicao.gd
@@ -3,11 +3,38 @@ extends CanvasLayer
 @onready var color_rect: ColorRect = $ColorRect
 @onready var sfx_transicao: AudioStreamPlayer = $SfxTransicao
 @onready var musica_fundo: AudioStreamPlayer = $MusicaFundo
+@onready var sfx_hover: AudioStreamPlayer = $SfxHover
+@onready var sfx_click: AudioStreamPlayer = $SfxClick
 
 func _ready() -> void:
 	color_rect.color = Color(0, 0, 0, 0.0)
 	color_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	
+	get_tree().node_added.connect(_on_no_adicionado)
+	
+	_varrer_arvore_existente(get_tree().root)
 
+func _varrer_arvore_existente(node: Node) -> void:
+	#Atualiza os nós que carregarem antes do autoload
+	_on_no_adicionado(node)
+	for filho in node.get_children():
+		_varrer_arvore_existente(filho)
+	
+func _on_no_adicionado(node: Node) -> void:
+	#Pega os nos futuros
+	if node is BaseButton:
+		if not node.mouse_entered.is_connected(tocar_som_hover):
+			node.mouse_entered.connect(tocar_som_hover)
+		if node.name != "Salvar":
+			if not node.button_down.is_connected(tocar_som_click):
+				node.button_down.connect(tocar_som_click)
+
+func tocar_som_hover() -> void:
+	sfx_hover.play()
+
+func tocar_som_click() -> void:
+	sfx_click.play()
+	
 func mudar_cena(caminho_da_cena: String, tocar_sfx: bool) -> void:
 	if tocar_sfx:
 		sfx_transicao.play()
